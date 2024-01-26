432d3be1718090fd869449058bb585a3f8deb68c
USER-PC2 <kirshner21102010@gmail.com>
2024-01-26 18:19:30 +0300

    saefesafszr

---
+++ list_generators.py
@@ -0,0 +1,24 @@
+# ciferki=[1,2,3,4,5,6,7,8,99,42,64,25,67,14,64,14,456,24,9,2,75,42,8,69,275]
+# result = [ciferk for ciferk in ciferki if ciferk >8 and ciferk < 69]
+# print(result)
+
+# names=['leo','max','kate','mag']
+#
+# names_upper=[name.upper() for name in names]
+# print(names_upper)
+#
+# names_m=[name for name in names if name[0]=='m']
+# print(names_m)
+
+import hanshlib
+hash_object=hashlib.md5(b'Hello World')
+print(hash_object.hexdigest())
+
+
+
+
+
+
+
+
+# copyright.Все права защищены от Кирилла и Антона
